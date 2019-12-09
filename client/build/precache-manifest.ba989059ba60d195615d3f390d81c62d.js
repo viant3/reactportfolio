9a15a0470e3505f479152376ec094e69cf1133a2
victor politano <viant3ucf@gmail.com>
2019-12-09 12:10:47 -0500

    start optimization

---
+++ client/build/precache-manifest.ba989059ba60d195615d3f390d81c62d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac840d8663db4a2d7b371f16fc9fffe9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "00f3ab11277f3a8815a5",
+    "url": "/static/css/2.0ad0672b.chunk.css"
+  },
+  {
+    "revision": "9f66ce01a44ec9bc0bf7",
+    "url": "/static/css/main.bf600af1.chunk.css"
+  },
+  {
+    "revision": "00f3ab11277f3a8815a5",
+    "url": "/static/js/2.f8ec088f.chunk.js"
+  },
+  {
+    "revision": "9f66ce01a44ec9bc0bf7",
+    "url": "/static/js/main.3d3e07ad.chunk.js"
+  },
+  {
+    "revision": "498a6017d9da24abad79",
+    "url": "/static/js/runtime-main.c58920cd.js"
+  },
+  {
+    "revision": "9cc64ca9eb02bedf1396cb32a30c6cd9",
+    "url": "/static/media/biohazzard.9cc64ca9.jpg"
+  },
+  {
+    "revision": "f5550e42e865f10f5ae07a26d9ca964c",
+    "url": "/static/media/claws.f5550e42.jpg"
+  },
+  {
+    "revision": "4e0eeceee633d186ab55f7357366e131",
+    "url": "/static/media/doctor.4e0eecee.jpg"
+  },
+  {
+    "revision": "608ab5fe4a45e2ac93febd3416b260f0",
+    "url": "/static/media/dust_scratches.608ab5fe.png"
+  },
+  {
+    "revision": "2c0e6a30cd9aa7f4906eafff386c4407",
+    "url": "/static/media/liri.2c0e6a30.jpg"
+  },
+  {
+    "revision": "b6a4bbf53d0fb01e0a0febdf0d629121",
+    "url": "/static/media/myhead.b6a4bbf5.jpg"
+  },
+  {
+    "revision": "4ccd84767f0652918af65c63cf246ce3",
+    "url": "/static/media/nsaBuild.4ccd8476.jpg"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  },
+  {
+    "revision": "f86d9bf34b5da8610c6a218e96667b68",
+    "url": "/static/media/react-bootstrap-carousel.f86d9bf3.svg"
+  },
+  {
+    "revision": "3f1da97aa62e4a2343adc004f713f073",
+    "url": "/static/media/triviaChuck.3f1da97a.jpg"
+  }
+]);
