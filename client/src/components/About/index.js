ad19b9400fe75a45ce9c2241b4816d8c918b7175
victor politano <viant3ucf@gmail.com>
2019-12-16 11:49:58 -0500

    added text to About component

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -6,8 +6,8 @@ import myHead from "../../pages/images/myhead.jpg"
 
 function About() {
 
-        return (
-            <div className="container-fluid mainBox2">
+    return (
+        <div className="container-fluid mainBox2">
 
             <div className="row">
                 <div className="col"><h2>About Me</h2></div>
@@ -17,28 +17,31 @@ function About() {
             <div className="row">
                 <div className="col-lg-3"> <img className="img-fluid imgSize2" src={myHead} alt="Victor Politano" /></div>
 
-                    <div className="col-lg-8 moveRight2">
-                        <p>
-                            My name is Victor Politano. I am a recent graduate of the University of Central Florida’s Full Stack
-                            Web Developer Bootcamp and have certifications in Web Design and Programming from Valencia College. I
-                            am skilled in the MERN Stack, (MongoDB, Express, React, and Node), and have strong fundamental HTML,
-                            CSS, and JavaScript skills. I am an excellent problem solver and I am excited to build applications with
-                            a team that shares my passion for coding.
-                                       </p>  
-                        <p>
-                            This site is a Full MERN Stack reproduction of my initial static HTML Portfolio page. The original page
+                <div className="col-lg-8 moveRight2">
+                    <p>
+                        My name is Victor Politano. I am a recent graduate of the University of Central Florida’s Full Stack
+                        Web Developer Bootcamp and have certifications in Web Design and Programming from Valencia College. I
+                        am skilled in the MERN Stack, (MongoDB, Express, React, and Node), and have strong fundamental HTML,
+                        CSS, and JavaScript skills. I am an excellent problem solver and I am excited to build applications with
+                        a team that shares my passion for coding.
+                                       </p>
+                    <p>
+                        This site is a Full MERN Stack reproduction of my initial static HTML Portfolio page. The original page
                             can be seen <a target="_blank" rel="noopener noreferrer" href="https://viant3.github.io/Portfolio/">HERE</a>.
                                           </p>
-                        <p> My GitHub repo for this sight is <a target="_blank" rel="noopener noreferrer" href="https://github.com/viant3/reactportfolio">HERE</a>.</p>
-                        
-                    </div>
-                   
+                    <p> My GitHub repo for this sight is <a target="_blank" rel="noopener noreferrer" href="https://github.com/viant3/reactportfolio">HERE</a>.
+                                         </p>
+
+                    <p>This site is and will always be a work-in-progress… keep checking back for new updates.
+                                        </p>
                 </div>
-               
+
+            </div>
+
         </div>
-             
-        );
-    };
+
+    );
+};
 
 
 export default About;
