2156a1c6a4eb7998bbd485b43abfd8e80fbae840
victor politano <viant3ucf@gmail.com>
2020-01-11 21:01:27 -0500

    small text change in about component

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -38,7 +38,7 @@ function About() {
                                       
                     <p>This site is and will always be a work-in-progress… keep checking back for new updates.
                                         </p>
-                    <p>Contact me via email<a href="mailto:viant3ucf@gmail.com"> HERE.</a>
+                    <p>Contact me via email at<a href="mailto:viant3ucf@gmail.com"> viant3UCF@gmail.com</a>
                                         </p>
             
                     
